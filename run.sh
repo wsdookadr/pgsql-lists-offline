64a79be71f2ab033c736d7607511c6072e9a02ae
Stefan Corneliu Petrea <stefan@garage-coding.com>
2018-09-22 22:20:37 +0300

    update

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./pgsql-lists-offline.sh -g pgsql-general
+./pgsql-lists-offline.sh -g pgsql-performance
+./pgsql-lists-offline.sh -g pgsql-hackers
+./pgsql-lists-offline.sh -g pgsql-admin
+./pgsql-lists-offline.sh -g pgsql-novice
+./pgsql-lists-offline.sh -g pgsql-sql
+./pgsql-lists-offline.sh -g pgsql-testers
+
